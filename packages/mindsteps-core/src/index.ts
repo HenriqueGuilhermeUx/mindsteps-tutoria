f8c23acda2eb2cd42645d31c8c0af615b001dea0
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-04 13:01:25 -0300

    core: export orchestrator and recommendation modules

--- packages/mindsteps-core/src/index.ts
+++ packages/mindsteps-core/src/index.ts
@@ -1,4 +1,7 @@
 export * from './types';
+export * from './learningEvents';
 export * from './pedagogicalEngine';
 export * from './learningMemory';
 export * from './cognitiveTwin';
+export * from './orchestrator';
+export * from './recommendationEngine';
