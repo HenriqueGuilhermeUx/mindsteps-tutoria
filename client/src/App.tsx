206b883767757a688e7d1769bd7c1b1c95c431a7
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-07 21:28:21 -0300

    client: add teacher dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,7 @@ import { OnboardingPage } from '@/pages/Onboarding'
 import { ChatPage } from '@/pages/Chat'
 import { ProfilePage } from '@/pages/Profile'
 import { LearningDashboardPage } from '@/pages/LearningDashboard'
+import { TeacherDashboardPage } from '@/pages/TeacherDashboard'
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useAuthStore()
@@ -24,46 +25,12 @@ function App() {
     <div className="min-h-screen flex flex-col bg-slate-50">
       <Routes>
         <Route path="/" element={<><Header /><HomePage /><Footer /></>} />
-
         <Route path="/auth" element={<AuthPage />} />
-
-        <Route path="/onboarding" element={
-          <ProtectedRoute>
-            <OnboardingPage />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/chat" element={
-          <ProtectedRoute>
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <div className="flex-1">
-                <ChatPage />
-              </div>
-            </div>
-          </ProtectedRoute>
-        } />
-
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <LearningDashboardPage />
-              <Footer />
-            </div>
-          </ProtectedRoute>
-        } />
-
-        <Route path="/perfil" element={
-          <ProtectedRoute>
-            <div className="flex flex-col min-h-screen">
-              <Header />
-              <ProfilePage />
-              <Footer />
-            </div>
-          </ProtectedRoute>
-        } />
-
+        <Route path="/onboarding" element={<ProtectedRoute><OnboardingPage /></ProtectedRoute>} />
+        <Route path="/chat" element={<ProtectedRoute><div className="flex flex-col min-h-screen"><Header /><div className="flex-1"><ChatPage /></div></div></ProtectedRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute><div className="flex flex-col min-h-screen"><Header /><LearningDashboardPage /><Footer /></div></ProtectedRoute>} />
+        <Route path="/professor" element={<ProtectedRoute><div className="flex flex-col min-h-screen"><Header /><TeacherDashboardPage /><Footer /></div></ProtectedRoute>} />
+        <Route path="/perfil" element={<ProtectedRoute><div className="flex flex-col min-h-screen"><Header /><ProfilePage /><Footer /></div></ProtectedRoute>} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </div>
