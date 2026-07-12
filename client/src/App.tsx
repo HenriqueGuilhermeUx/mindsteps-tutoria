f93c74230d46c94ab70769a8dbe88c42cdd5f6c2
HenriqueGuilhermeUx <henriquecampos66@gmail.com>
2026-07-12 18:14:21 -0300

    client: add learning genome route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -27,6 +27,7 @@ import { CompanyPage } from '@/pages/Company'
 import { RoadmapPage } from '@/pages/Roadmap'
 import { SafetyPage } from '@/pages/Safety'
 import { AccessibilityPage } from '@/pages/Accessibility'
+import { LearningGenomePage } from '@/pages/LearningGenome'
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
   const { isAuthenticated } = useAuthStore()
@@ -47,6 +48,7 @@ function App() {
         <Route path="/roadmap" element={<><Header /><RoadmapPage /><Footer /></>} />
         <Route path="/seguranca" element={<><Header /><SafetyPage /><Footer /></>} />
         <Route path="/acessibilidade" element={<><Header /><AccessibilityPage /><Footer /></>} />
+        <Route path="/genoma" element={<><Header /><LearningGenomePage /><Footer /></>} />
         <Route path="/testes" element={<><Header /><TestLabPage /><Footer /></>} />
         <Route path="/protocolo" element={<><Header /><TestProtocolPage /><Footer /></>} />
         <Route path="/inteligencia" element={<><Header /><IntelligenceLabPage /><Footer /></>} />
